b9dbffc7cbd22c530772459081c0d846255c0e98
adrifern48 <adrifern48@gmail.com>
2021-05-21 03:26:07 +0000

    check for green light before printing that entry was granted

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -84,11 +84,13 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
     lck.lock();
+
+    while (TrafficLightPhase::RED == _trafficLight.getCurrentPhase())
+        _trafficLight.waitForGreen();
+
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
-    if (TrafficLightPhase::RED == _trafficLight.getCurrentPhase())
-        _trafficLight.waitForGreen();
 
     lck.unlock();
 }
